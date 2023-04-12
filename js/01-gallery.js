307fec51ee07e3ade75f48df3222a566aef4960f
NGanch <86801593+NGanch@users.noreply.github.com>
2023-04-12 23:18:48 +0100

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -48,62 +48,6 @@ const instance = basicLightbox.create(
 instance.close()
 // =============================
 
-// const callGallery = createImageCard(galleryItems);
- 
-// const markup = galleryItems.map(({id, preview, original, description}) => 
-//   `<li data-card-id = "${id}" class="gallery__item js-target">
-//    <a class = "js-target" href ="${preview}">
-//    <img
-//    class="gallery__image js-target"
-//    href ="${original}"
-//    src="${preview}"
-//    data-source="${original}"
-//    alt="${description}"
-//  />
-// </a> </li>`
-//     ).join('');
- 
-  
-// container.insertAdjacentHTML('beforeend', markup);
-// console.log(container)
-
-// container.addEventListener('click', onClick);
-
-
-//     function onClick(evt){
-//         const galleryimg = evt.target.classList.contains('.js-target');
-        
-//         if(!galleryimg){
-//          console.log('1')
-//         }
-//             // console.log(evt.target);
-//             console.log('2')
-        
-    
-//     }
-
-
-
-//  // модальне вікно
-// const instance = basicLightbox.create(`
-// <div>
-// <li class="gallery-item js-target">
-// <a class = "gallery_image js-target" href ="${currentItem.preview}">
-// <img
-// class="gallery__image js-target"
-// href ="${currentItem.original}"
-// src="${currentItem.preview}"
-// data-source="${currentItem.original}"
-// alt="${currentItem.description}"
-// />
-// </a> </li>
-// </div>
-// `)
-
-// instance.show()
-// // модальне вікно
-
-// }
 
 
 
