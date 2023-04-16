b395dbc943ee6bf94b5cbcf8a106ce90c64e3608
NGanch <86801593+NGanch@users.noreply.github.com>
2023-04-16 23:11:34 +0100

    Finish homework

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,11 +2,10 @@ import { galleryItems } from './gallery-items.js';
 
 const container = document.querySelector('.gallery');
 
-// =============================
 function createImageCard(array) {
     const markup = array.map(({preview, original, description}) => {
     return `<li class="gallery__item">
-   <a class = "" href ="${preview}">
+   <a class = "gallery__link" href ="${preview}">
    <img
    class= "gallery__image"
    href ="${original}"
@@ -17,36 +16,25 @@ function createImageCard(array) {
 </a> </li>`
     }).join('');
     return markup;
-  
 }
+
 container.insertAdjacentHTML('beforeend', createImageCard(galleryItems));
 
 container.addEventListener('click', onClick);
 function onClick(evt){
     evt.preventDefault();
-    const galleryImg = evt.target.classList.contains('.gallery__image');
+    const galleryImg = evt.target.classList.contains('gallery__image');
     
     if(!galleryImg){
      return;
     }
-            //  // модальне вікно
+
  const instance = basicLightbox.create(
     `<img src="${evt.target.dataset.source}">`);
             
     instance.show();
-// модальне вікно
-}
-   
-    
- 
-const instance = basicLightbox.create(
-    `<img src="${evt.target.dataset.source}">`
-, {
-	closable: false
-})
 
-instance.close()
-// =============================
+}
 
 
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const container = document.querySelector('.gallery');
+
+function createImageCard(array){
+    const markup = array.map(({preview, original, description}) => {
+       return  `<li class="gallery__item">
+        <a class="gallery__link" href="${preview}">
+           <img class="gallery__image"
+            src="${original}"
+             alt="${description}" />
+        </a>
+     </li> `
+    }).join('');
+    return markup;
+}
+
+container.insertAdjacentHTML('beforeend', createImageCard(galleryItems));
+
+
+let gallery = new SimpleLightbox(".gallery a", { captionsData: "alt"}); 
